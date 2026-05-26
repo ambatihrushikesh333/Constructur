14db25a13bd703ecee965cc3e4f2cdc10a825e1e
ADMIN <ADMIN@H-RUSHI-KESH>
2026-05-26 11:50:41 +0530

    git

--- Constructur/Program.cs
+++ Constructur/Program.cs
@@ -1,40 +1,40 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
 
-namespace copy_constructors
-{
-    class student
-    {
-        public int Sid;
-        public string Sname;
+//namespace copy_constructors
+//{
+//    class student
+//    {
+//        public int Sid;
+//        public string Sname;
 
-        public student(int sid, string sname)
-        {
-            Sid = sid;
-            Sname = sname;
-        }
-        public student(student stu)
-        {
-            Sid = stu.Sid;
-            Sname = stu.Sname;
-        }
-        public void studentdisplay()
-        {
-            Console.WriteLine("Student id is " + Sid);
-            Console.WriteLine("Student name is " + Sname);
-        }
-    }
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-            student stu1 = new student(101, "John");
-            stu1.studentdisplay();
-            student stu2 = new student(stu1);
-            stu2.studentdisplay();
-        }
-    }
-}
+//        public student(int sid, string sname)
+//        {
+//            Sid = sid;
+//            Sname = sname;
+//        }
+//        public student(student stu)
+//        {
+//            Sid = stu.Sid;
+//            Sname = stu.Sname;
+//        }
+//        public void studentdisplay()
+//        {
+//            Console.WriteLine("Student id is " + Sid);
+//            Console.WriteLine("Student name is " + Sname);
+//        }
+//    }
+//    internal class Program
+//    {
+//        static void Main(string[] args)
+//        {
+//            student stu1 = new student(101, "John");
+//            stu1.studentdisplay();
+//            student stu2 = new student(stu1);
+//            stu2.studentdisplay();
+//        }
+//    }
+//}
---
+++ Emp/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Emp
+{
+    public class Person
+    {
+        public int Age;
+        public string Name;
+
+        public Person(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+        public Person(Person previousperson)
+        {
+            Name = previousperson.Name;
+            Age = previousperson.Age;
+        }
+        public string Details()
+        {
+            return "Name is " + Name + " and age is " + Age;
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person1 = new Person("Jay", 30);
+
+            Person person2 = new Person(person1);
+            
+            person1.Age = 34;
+            person2.Age = 23;
+
+            person2.Name = "Jack";
+            Console.WriteLine(person1.Details());   
+            Console.WriteLine(person2.Details());
+        }
+    }
+}
